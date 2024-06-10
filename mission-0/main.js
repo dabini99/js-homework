03cd079e1d47f85e04d8a8badf213466e993127b
dabini99 <djssk8475@gmail.com>
2024-06-10 09:25:13 +0900

    해당키값을 가지고 있는지 없으면 에러를 반환하기

--- mission-0/main.js
+++ mission-0/main.js
@@ -20,6 +20,8 @@ function getValueAtObject(obj, key) {
   if(typeof obj === 'object'){
     if(Object.prototype.hasOwnProperty.call(obj,key)){
       return obj[key];
+    }else{
+      throw new Error(`getValueAtObject 함수의 해당 ${key}가 존재하지 않습니다`);
     }
     
   }else{
