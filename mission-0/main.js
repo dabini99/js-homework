2ce12a0c34fe0551b6d89fef86e674d35502c324
dabini99 <djssk8475@gmail.com>
2024-06-10 09:35:42 +0900

    //2. 0보다 크거나 같음, && 배열의 길이보다 작을 때

--- mission-0/main.js
+++ mission-0/main.js
@@ -45,10 +45,15 @@ console.log(getValueAtObject(person, 'country')); // Error !
 
 const numbers = [10, 20, 30, 40, 50];
 //1.arr변수가 배열인지 확인하기 Array.isArray함수를 사용해서
+//2. 0보다 크거나 같음, && 배열의 길이보다 작을 때
 
 function getNumberAtArray(arr, index) {
   if(Array.isArray(arr)){
-    return arr.length;
+    if(index >= 0 && index < arr.length){
+      return arr[index];
+    }else{
+      throw new Error('배열에 없는 index입니다')
+    }
   }else{
     throw new TypeError('getValueAtObject 함수의 첫번째 인수는 배열 타입이어야 합니다');
   }
