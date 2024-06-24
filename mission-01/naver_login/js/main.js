742ec5e719ad4be62b8d5a7afdc34b8fd303227b
dabini99 <djssk8475@gmail.com>
2024-06-24 17:22:00 +0900

    코드리뷰보고 작성

--- mission-01/naver_login/js/main.js
+++ mission-01/naver_login/js/main.js
@@ -13,7 +13,7 @@ const user = {
 5. 두 값이 일치 한다면 다음 페이지(welcome.html)로 이동 
 
 */
-input.addEventListener('input');
+
 
 function emailReg(text){
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -25,9 +25,62 @@ function pwReg(text){
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
+const emailInPut = document.querySelector('#userEmail');
+const passWordInPut = document.querySelector('#userPassword');
+const logInBtn = document.querySelector('.btn-login');
+
+let emailCheckPass = false;
+let pwCheckPass = false;
+
+function handleEmailCheck(){
+  const value = this.vlaue;
+  if(emailReg(value)){
+    this.classList.remove('is--invlid');
+    emailCheckPass = true;
+  }else{
+    this.classList.add('is--invalid');
+    emailCheckPass = false;
+  }
+}
+
+function handlePasswordCheck(){
+  const value = this.vlaue;
+  if(pwReg(value)){
+    this.classList.remove('is--invlid');
+    pwCheckPass = true;
+  }else{
+    this.classList.add('is--invalid');
+    pwCheckPass = false;
+  }
+}
+
+function handleLogin(e){
+  e.preventDefault();
+
+  if(emailCheckPass && pwCheckPass){
+    try{
+      const id = emailInPut.value;
+      const pw = passWordInPut.value;
+
+      const getUserId = user.id;
+      const getUserPw = user.pw;
+
+      if(id === getUserId && pw === getUserPw){
+        location.href = 'welcome.html';
+      }
+    }catch{
+
+    }
+  }
+
+}
 
 
 
+
+emailInPut.addEventListener('input',handleEmailCheck);
+passWordInPut.addEventListener('input',handlePasswordCheck);
+logInBtn.addEventListener('input',handleLogin);
 
 
 
